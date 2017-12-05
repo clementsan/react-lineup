613d186ea349387a1eb89b9a37a1a079fb716fb8
Clement Vachet <cvachet@sci.utah.edu>
2017-12-05 12:15:01 -0700

    Enable LineUp instead of Taggle, use CNV data

--- src/LineUpExample.js
+++ src/LineUpExample.js
@@ -19,16 +19,17 @@ class LineUpExample extends Component {
   launchLineUp () {
     return (element) => {
       if (element) {
-      	this.createLineUp(element);
+      	//this.createTaggle(element);
+        this.createLineUp(element);
       }
     }
   }
  
  
-  createLineUp (element) {
+  createTaggle (element) {
   	return new Promise(function (resolve, reject) {
-		  const JSONdataset = iris_json2;
-		  //const dataset = testCNVTable;
+		  //const JSONdataset = iris_json2;
+		  const JSONdataset = testCNVTable;
 		  console.log("JSONdataset",JSONdataset);
 		  const LineUp = window.LineUpJS;
 		  console.log("LineUp",LineUp);
@@ -43,6 +44,23 @@ class LineUpExample extends Component {
 		});
   }
 
+  createLineUp (element) {
+    return new Promise(function (resolve, reject) {
+      //const JSONdataset = iris_json2;
+      const JSONdataset = testCNVTable;
+      console.log("JSONdataset",JSONdataset);
+      const LineUp = window.LineUpJS;
+      console.log("LineUp",LineUp);
+      const ColumnDescriptions = window.LineUpJS.deriveColumnDescriptions(JSONdataset);
+      const LineUpdata = window.LineUpJS.createLocalStorage(JSONdataset, ColumnDescriptions);
+      LineUpdata.deriveDefault();
+      console.log("LineUpdata",LineUpdata);
+      var instance = window.LineUpJS.create(LineUpdata, element);
+      console.log("LineUpinstance",instance);
+      instance.update();
+      resolve(instance);
+    });
+  }
 
     render () {
     return <div ref={this.launchLineUp()}></div>
